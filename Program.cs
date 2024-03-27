c852a3d486ef7434a34fe05e71e7dda4d634fe2a
Clarenskyy <150917878+Clarenskyy@users.noreply.github.com>
2024-03-27 10:49:12 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,63 +14,74 @@ namespace ConversionMethod
                 "\n4. d-mths (days to months)" +
                 "\nSelect (1-4): ");
             String UserInput = Console.ReadLine();
-            if (UserInput == "1") 
-            {
-                Console.Write("Enter the second/s to convert: ");
-                double UserNum = Convert.ToInt32(Console.ReadLine());
-                double min = 60;
-                double Output;
-                Output = UserNum / min;
 
-                Console.WriteLine(UserNum + " second/s in minute/s is: " + Output + " minute/s" );
-            }
-            else
-            {
-                if (UserInput == "2")
+
+                if (UserInput == "1")
                 {
-                    //m-h
-                    Console.Write("Enter the minute/s to convert: ");
-                    double UserNum = Convert.ToInt32(Console.ReadLine());
-                    double hour = 60;
-                    double Output;
-                    Output = UserNum / hour;
+                    Console.Write("Enter the second/s to convert: ");
+                    int UserNum = Convert.ToInt32(Console.ReadLine());
+                    int min = 60;
+                    int Output;
+                    int output2;
+                    Output = UserNum / min;
+                    output2 = UserNum % min;
 
-                    Console.WriteLine(UserNum + " minute/s in hour/s is: " + Output + " hour/s");
+                    Console.WriteLine(UserNum + " second/s in minute/s is: " + Output + " minute/s and " + output2 + " second/s");
                 }
                 else
                 {
-                    if (UserInput == "3") 
+                    if (UserInput == "2")
                     {
-                        //h-d
-                        Console.Write("Enter the hour/s to convert: ");
-                        double UserNum = Convert.ToInt32(Console.ReadLine());
-                        double days = 24;
-                        double Output;
-                        Output = UserNum / days;
-
-                        Console.WriteLine(UserNum + " hour/s in day/s is: " + Output + " day/s");
+                        //m-h
+                        Console.Write("Enter the minute/s to convert: ");
+                        int UserNum = Convert.ToInt32(Console.ReadLine());
+                        int hour = 60;
+                        int Output;
+                        Output = UserNum / hour;
+                        int output2;
+                        Output = UserNum / hour;
+                        output2 = UserNum % hour; 
+                    Console.WriteLine(UserNum + " minute/s in hour/s is: " + Output + " hour/s and " + output2 + " minute/s");
                     }
                     else
                     {
-                        if (UserInput == "4")
-                            {
-                            // d-mths
-                            Console.Write("Enter the day/s to convert: ");
-                            double UserNum = Convert.ToInt32(Console.ReadLine());
-                            double months = 30;
-                            double Output;
-                            Output = UserNum / months;
+                        if (UserInput == "3")
+                        {
+                            //h-d
+                            Console.Write("Enter the hour/s to convert: ");
+                            int UserNum = Convert.ToInt32(Console.ReadLine());
+                            int days = 24;
+                            int Output;
+                            int output2;
+                            Output = UserNum / days;
+                            output2 = UserNum % days;   
 
-                            Console.WriteLine(UserNum + " day/s in month/s is: " + Output + " months/s");
+                            Console.WriteLine(UserNum + " hour/s in day/s is approximately: " + Output + " day/s and " + output2 + " hour/s");
                         }
                         else
                         {
-                            Console.WriteLine("Invalid Option, input ONLY 1-4.");
-                            return;
+                            if (UserInput == "4")
+                            {
+                                // d-mths
+                                Console.Write("Enter the day/s to convert: ");
+                                int UserNum = Convert.ToInt32(Console.ReadLine());
+                                int months = 30;
+                                int Output;
+                                int Output2;
+                                Output2 = UserNum % months;
+                                Output = UserNum / months;
+
+                                Console.WriteLine(UserNum + " day/s in month/s is approximately: " + Output + " months/s and " + Output2 + " day/s");
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Option, input ONLY 1-4.");
+                                return;
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
